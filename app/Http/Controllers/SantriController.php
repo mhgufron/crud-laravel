bc21872e219d5f3fbc5f579b573bd581d5d52dcb
mhgufron <mgufron223@gmail.com>
2018-04-09 11:00:38 +0700

    SantriController store controller

--- app/Http/Controllers/SantriController.php
+++ app/Http/Controllers/SantriController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Santri;
 use Illuminate\Http\Request;
 
 class SantriController extends Controller
@@ -9,5 +10,20 @@ class SantriController extends Controller
     public function create()
     {
         return view('santri.create');
+    }
+
+    public function store(Request $roquest)
+    {
+        $nama           = $request->nama;
+        $umur           = $request->umur;
+        $alamat         = $request->alamat;
+        $jenis_kelamin  = $request->jenis_kelamin;
+
+        Santri::create([
+            'nama'              => $nama,
+            'umur'              => $umur,
+            'alamat'            => $alamat,
+            'jenis_kelamin'     => $jenis_kelamin,
+        ]);
     }
 }
