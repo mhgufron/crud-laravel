be323a4c4b2656114ce6c48e9d550369936bd6c1
mhgufron <mgufron223@gmail.com>
2018-04-11 08:46:34 +0700

    edit dan update crud

--- resources/views/santri/edit.blade.php
+++ resources/views/santri/edit.blade.php
@@ -2,25 +2,25 @@
 
 <!-- <form action="santri" method="post"> -->
 <!-- <form action="{{ url('santri') }}" method="post"> -->
-<form action="{{ route('santri.update', Santri->id) }}" method="post">
+<form action="{{ route('santri.update', $santri->id) }}" method="post">
     <!-- crsf token -->
 
     {{-- {{ csrf_field() }} --}}
 
     @csrf
-    <input type="text" name="nama" placeholder="Edit Name" value=""><br>
+    <input type="text" name="nama" placeholder="Edit Name" value="{{ $santri->nama }}"><br>
     @if ($errors->has('nama'))
         <strong>{{ $errors->first('nama') }}</strong><br>
     @endif
-    <input type="text" name="umur" placeholder="Edit Umur" value=""><br>
+    <input type="text" name="umur" placeholder="Edit Umur" value="{{ $santri->umur }}"><br>
     @if ($errors->has('umur'))
         <strong>{{ $errors->first('umur') }}</strong><br>
     @endif
-    <textarea name="alamat" rows="5" cols="30" placeholder="Edit Address" value=""></textarea><br>
+    <textarea name="alamat" rows="5" cols="30" placeholder="Edit Address" value="">{{ $santri->alamat }}</textarea><br>
     @if ($errors->has('alamat'))
         <strong>{{ $errors->first('alamat') }}</strong><br>
     @endif
-    <input type="text" name="jenis_kelamin" placeholder="Edit Gender" value=""><br>
+    <input type="text" name="jenis_kelamin" placeholder="Edit Gender" value="{{ $santri->jenis_kelamin }}"><br>
     @if ($errors->has('jenis_kelamin'))
         <strong>{{ $errors->first('jenis_kelamin') }}</strong><br>
     @endif
